af9cdd879d47f2992ba5b48f9afc351068942184
DhanushkaTa <dhanushka999ta@gmail.com>
2024-03-28 09:30:56 +0530

    create card

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,11 +1,44 @@
-
+import { TbEdit } from "react-icons/tb";
+import { MdDelete } from "react-icons/md";
 
 function Card() {
 
     return(
-        <section className={"w-[200px] h-[230px] bg-white rounded-md m-auto border-2 border-gray-300"}>
+        <section className={"w-[200px] h-[230px] relative p-2 pb-5 bg-white rounded-md m-auto border-2 border-gray-300 cursor-pointer"}>
 
 
+            <div className={"absolute top-0 left-0 right-0 w-full min-h-[50px] h-max group"}>
+
+                {/**/}
+
+                <div className={"w-full h-[0px] group-hover:h-[50px] transition-all bg-gradient-to-b from-0% from-red-600/20 to-transparent flex items-center justify-center rounded-b-[50%] "}>
+
+                    <MdDelete size={22} className={"opacity-0 group-hover:opacity-100 "} onClick={() => alert("123")}/>
+
+                </div>
+
+            </div>
+
+            <div className={"absolute bottom-3 right-3 bg-black p-2 w-max rounded-md text-white" +
+                " hover:bg-[#D0D1D3]  hover:text-black"}>
+
+                <TbEdit size={20}/>
+
+            </div>
+
+            <div className={"w-full h-full overflow-hidden"}>
+
+                <div className={"text-black font-Lex text-2xl font-[600] w-full h-full text-ellipsis  overflow-hidden"}>
+
+                    How to Create React app
+
+                </div>
+
+            </div>
+
+            <div className={"text-gray-400 font-Lex text-sm font-[600] absolute bottom-2 left-3"}>
+                2024-03-17
+            </div>
 
 
         </section>
